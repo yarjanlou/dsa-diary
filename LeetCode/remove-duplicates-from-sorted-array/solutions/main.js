456f61de114a3995cfd153ae32f991569b3e95a1
reihan-yr <r.yarjanlou@gmail.com>
2025-11-08 23:04:37 +0330

    chore: use two-pointer approach for removing duplicates

--- LeetCode/remove-duplicates-from-sorted-array/solutions/main.js
+++ LeetCode/remove-duplicates-from-sorted-array/solutions/main.js
@@ -4,13 +4,16 @@
  */
 
 function removeDuplicates(nums) {
-  let i = 1;
-  while (i < nums.length) {
-    if (nums[i] === nums[i - 1]) {
-      nums.splice(i, 1);
-    } else {
+  if (nums.length === 0) return 0;
+
+  let i = 0;
+
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
       i++;
+      nums[i] = nums[j];
     }
   }
-  return nums.length;
+
+  return i + 1;
 }
