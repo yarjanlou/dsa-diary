9875a26cda90cb92c1dfe2b7dbf01fc326fb009c
Reihan <95295595+yarjanlou@users.noreply.github.com>
2026-01-04 13:43:12 +0330

    chore: add js solution for balanced binary tree (#12)
    * chore: add js solution for balanced binary tree
    
    * chore: update js solution for balanced binary tree

---
+++ LeetCode/balanced-binary-tree/solutions/main_naive.js
@@ -0,0 +1,17 @@
+var isBalanced = function (root) {
+  return height(root) !== -1;
+};
+
+function height(node) {
+  if (node === null) return 0;
+
+  const leftHeight = height(node.left);
+  if (leftHeight === -1) return -1;
+
+  const rightHeight = height(node.right);
+  if (rightHeight === -1) return -1;
+
+  if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+
+  return Math.max(leftHeight, rightHeight) + 1;
+}
