c174b8c9e00ef02f3a639d814f8335857308ff13
yarjanlou <r.yarjanlou@gmail.com>
2026-08-29 09:56:25 +0330

    refactor: simplify plus one JavaScript solution

--- LeetCode/plus-one/solutions/main.js
+++ LeetCode/plus-one/solutions/main.js
@@ -5,16 +5,13 @@
 
 var plusOne = function (digits) {
   for (let i = digits.length - 1; i >= 0; i--) {
-    if (digits[i] + 1 != 10) {
-      digits[i] = digits[i] + 1;
+    if (digits[i] < 9) {
+      digits[i]++;
       return digits;
     }
     digits[i] = 0;
   }
 
-  if (digits[0] == 0) {
-    digits = [1, ...digits];
-  }
-
+  digits.unshift(1);
   return digits;
 };
